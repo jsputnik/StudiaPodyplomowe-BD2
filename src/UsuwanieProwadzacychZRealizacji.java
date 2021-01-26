a3594bc9178c63d7baddcf32db8cbd3891a15ab5
pionierrrrr77777 <76958042+pionierrrrr77777@users.noreply.github.com>
2021-01-26 19:33:53 +0100

    Add files via upload

--- src/UsuwanieProwadzacychZRealizacji.java
+++ src/UsuwanieProwadzacychZRealizacji.java
@@ -83,5 +83,39 @@ public class UsuwanieProwadzacychZRealizacji {
 	{
 		this.realizacjaPrzedmiotu = realizacjaPrzedmiotu;
 	}
+	
+	public void update() {
+		Connections connect = new Connections();
+		
+		try{
+			connect.setConnection();
+			
+			ResultSet RsPrzypis = connect.connectionMakeRead("SELECT Rpr.id_realizacji, nazwa, kod_przedmiotu, numer_semestru, P.id_pracownika, imie, nazwisko "
+				+	"FROM Przypisy_prow_do_real PR "
+				+	"JOIN Prowadzacy PT ON PR.id_pracownika = PT.id_pracownika "
+				+	"JOIN Pracownicy P ON PT.id_pracownika = P.id_pracownika "
+				+	"JOIN Realizacje_przedmiotow RPr ON RPr.id_realizacji = PR.id_realizacji "
+				+	"JOIN Przedmioty Pr ON RPr.id_przedmiotu = Pr.id_przedmiotu");
+			
+			while (RsPrzypis.next()) {
+				Prowadzacy prowadzacy = new Prowadzacy(RsPrzypis.getInt(5), RsPrzypis.getString(6), RsPrzypis.getString(7));
+				Przedmiot przedmiot = new Przedmiot(RsPrzypis.getString(2),  RsPrzypis.getString(3));
+				RealizacjaPrzedmiotu realizacjaPrzedmiotu = new RealizacjaPrzedmiotu(RsPrzypis.getInt(1), przedmiot , RsPrzypis.getString(4));
+				PrzypisProwadzacegoDoRealizacji przypisProwadzacegoDoRealizacji = new PrzypisProwadzacegoDoRealizacji(prowadzacy, realizacjaPrzedmiotu);
+				listaPrzypisowProwadzacychDoRealizacji.dodajPrzypis(przypisProwadzacegoDoRealizacji);
+			}
+			
+			connect.closeConnection();
+		}
+		catch (SQLException eSQL) 
+		{
+			System.out.println("Blad przetwarzania SQL");
+		}
+		catch (IOException eIO) 
+		{
+			System.out.println("Nie mozna otworzyc pliku z parametrami polaczenia");
+		}
+		
+	}
 
 }
